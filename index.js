dfd3ad91efe2cb44841797b00d2f0b55592fe1a6
Thulane Mfeketho <mfekethothulane@gmail.comm>
2022-09-14 11:53:17 +0200

    update

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 
+function dataUser() {
+    username = document.getElementById("username").value;
+    email = document.getElementById("email").value;
+    idNumber = document.getElementById("idNumber").value;
 
+    localStorage.setItem("username" , username)
+    localStorage.setItem("email" , email)
+    localStorage.setItem("idNumber" , idNumber)
+    console.log(localStorage)  
+    
+    alert( "well done")
+}
 
 
